727d2c8593e73a56d4c7b4670ac40c0b36e80357
ChenShangfeng <9033986+whubrucechen@user.noreply.gitee.com>
2026-03-03 22:24:55 +0800

    chore: add gitignore and verify build

--- src/background/sync-manager.ts
+++ src/background/sync-manager.ts
@@ -67,7 +67,7 @@ export class SyncManager {
     let hash = 0;
     for (let i = 0; i < content.length; i++) {
       const char = content.charCodeAt(i);
-      hash = ((hash << ) - hash) + char;
+      hash = ((hash << 5) - hash) + char;
       hash = hash & hash;
     }
     return hash.toString(36);
