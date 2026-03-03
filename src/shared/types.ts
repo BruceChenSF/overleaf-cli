2b5b43e8db15ee9f7cb9e48b120c1993aaff6661
ChenShangfeng <9033986+whubrucechen@user.noreply.gitee.com>
2026-03-03 22:18:41 +0800

    feat: add shared type definitions

---
+++ src/shared/types.ts
@@ -0,0 +1,49 @@
+// Message types for extension communication
+export type ExtensionMessage =
+  | OpenTerminalMessage
+  | SyncFileMessage
+  | FetchFilesMessage
+  | TerminalReadyMessage;
+
+export interface OpenTerminalMessage {
+  type: 'OPEN_TERMINAL';
+  projectId: string;
+  projectUrl: string;
+}
+
+export interface SyncFileMessage {
+  type: 'SYNC_FILE';
+  projectId: string;
+  filepath: string;
+  content: string;
+}
+
+export interface FetchFilesMessage {
+  type: 'FETCH_FILES';
+  projectId: string;
+}
+
+export interface TerminalReadyMessage {
+  type: 'TERMINAL_READY';
+  windowId: number;
+}
+
+// Overleaf API types
+export interface OverleafDoc {
+  _id: string;
+  name: string;
+  path: string;
+}
+
+export interface OverleafProject {
+  _id: string;
+  name: string;
+}
+
+// Sync state
+export interface FileSyncState {
+  filepath: string;
+  docId: string;
+  lastSyncedAt: number;
+  localHash: string;
+}
